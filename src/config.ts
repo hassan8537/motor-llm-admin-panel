5ea727c52463718953e65df134b7deff62aeef6a
hassan8537 <hassan.m.8537@gmail.com>
2025-07-28 03:13:52 +0500

    Admin

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-// const BASE_URL =
-//   "https://mdzogc2o5ieq54ebed6oelcmfe0larkk.lambda-url.us-east-1.on.aws";
+const BASE_URL =
+  "https://mdzogc2o5ieq54ebed6oelcmfe0larkk.lambda-url.us-east-1.on.aws";
 
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
 
 export default BASE_URL;
